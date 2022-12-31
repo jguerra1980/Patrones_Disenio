3b7a91084360331968945f8ec3f077d7822e694e
JoseGuerra <solmovsa@gmail.com>
2022-12-31 15:55:53 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//var singleton = new Singleton.Singleton(); // esto no permite por que el constructor está privado
+
+//var log = new Singleton.log(); // esto no permite por que el constructor está privado
+
+var log = Singleton.Singleton.Log.Instance;
+log.guardarlog("J");
+log.guardarlog("o");
+log.guardarlog("s");
+log.guardarlog("e");
+
---
+++ Singleton/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Singleton
+{
+    public  class Log
+    {
+        private readonly static Log _instance = new Log();
+        private string path = "log.txt";
+
+        public static Log Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private Log()
+        {
+        
+        }
+        public void guardarlog(string mensaje)
+        {
+            File.AppendAllText(path, mensaje + Environment.NewLine);
+
+        }
+
+
+
+
+    }
+}
